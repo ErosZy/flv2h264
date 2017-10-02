2a6303edd32eeda9efa43e7b76e38eb910149966
zyEros <395693101@qq.com>
2017-10-02 23:33:29 +0800

    change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,7 @@ let config = getConfig({
 let jsconf = config.module.rules[0];
 delete jsconf.exclude;
 
+config.output.library = 'FLV2H264';
+config.output.libraryTarget = 'umd';
+
 module.exports = config;
